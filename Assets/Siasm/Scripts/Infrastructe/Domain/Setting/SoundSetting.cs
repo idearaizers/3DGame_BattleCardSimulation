f36ce4597d4ef0f8d80207bd8b4f191124869f02
Renoman <idearaizers@gmail.com>
2025-01-12 13:07:22 +0900

    add

--- Assets/Siasm/Scripts/Infrastructe/Domain/Setting/SoundSetting.cs
+++ Assets/Siasm/Scripts/Infrastructe/Domain/Setting/SoundSetting.cs
@@ -5,9 +5,9 @@ namespace Siasm
     [Serializable]
     public class SoundSetting
     {
-        public int MasterVolumeNumber = 5;
-        public int BGMVolumeNumber = 5;
-        public int SEVolumeNumber = 7;
+        public int MasterVolumeNumber = 3;
+        public int BGMVolumeNumber = 3;
+        public int SEVolumeNumber = 9;
         public int VoiceVolumeNumber = 10;
     }
 }
