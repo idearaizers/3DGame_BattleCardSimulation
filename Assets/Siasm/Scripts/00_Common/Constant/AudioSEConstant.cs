411f6e18cbb4ca50cc4da2084fe95737c49b6cfe
Renoman <idearaizers@gmail.com>
2025-01-12 22:02:00 +0900

    add

--- Assets/Siasm/Scripts/00_Common/Constant/AudioSEConstant.cs
+++ Assets/Siasm/Scripts/00_Common/Constant/AudioSEConstant.cs
@@ -1,5 +1,9 @@
 namespace Siasm
 {
+    /// <summary>
+    /// NOTE: 共通のものとバトルのものが混ざっていたり使用していないものもあるようで管理方法を見直した方がいいかも
+    /// NOTE: ScriptableObjectで指定のためidを明示的に指定した方がいいかも
+    /// </summary>
     public enum AudioSEType
     {
         // 共通関連のもの
@@ -9,31 +13,13 @@ namespace Siasm
         OnMouseCursor,
 
         // バトル関連のもの
-        OnMouseCard,    // 
-        HandDraw,       // 
-        BattleStart,    // 
-        Strike1,        // 後でエネミー側に持たせる
-        BattleDraw,     // 
-        Guard1,         // 
-        FinishBattle,   // 
-        MatchingView    // 
-    }
-
-    /// <summary>
-    /// 楽曲サイト
-    /// https://soundeffect-lab.info/sound/button/
-    /// </summary>
-    public static class AudioSEConstant
-    {
-        // public static readonly string DecideSE = "decide_button_19";  // ファイルを開くような音
-        // public static readonly string CancelSE = "decide_button_7";   // カチャ
-        // public static readonly string CursorOnSE = "move_cursor_5";
-        // public static readonly string OnPointCardSE = "on_point_card_1";    // カンッ
-
-        // 他候補・未使用のもの
-        // public static readonly string FideOpenSE = "decide_button_18";    // カシャーン
-        // public static readonly string DecideSE = "decide_button_2";    // カンッ
-        // public static readonly string DecideSE = "decide_button_6";    // ジリッ
-        // public static readonly string OnPointCardSE = "on_point_card_2";    // 鞘を持つ
+        OnMouseCard,
+        HandDraw,
+        BattleStart,
+        Strike1,
+        BattleDraw,
+        Guard1,
+        FinishBattle,
+        MatchingView
     }
 }
