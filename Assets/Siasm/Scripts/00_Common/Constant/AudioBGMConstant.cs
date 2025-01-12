113405e78a00b07973ed006d61841bca77f2274a
Renoman <idearaizers@gmail.com>
2025-01-12 21:58:52 +0900

    add

--- Assets/Siasm/Scripts/00_Common/Constant/AudioBGMConstant.cs
+++ Assets/Siasm/Scripts/00_Common/Constant/AudioBGMConstant.cs
@@ -1,32 +1,14 @@
 namespace Siasm
 {
+    /// <summary>
+    /// AudioBGMTypeAudioClipDictionaryで再生するBGMを指定する際に使用
+    /// NOTE: 拡張性を持たせるためにシーン別のScriptableObjectでの管理に変えた方がいいかも
+    /// </summary>
     public enum AudioBGMType
     {
         None = 0,
         TopScene,
         MainScene,
         BattleScene
-    }
-
-    /// <summary>
-    /// 楽曲サイト
-    /// https://musmus.main.jp/music_img4.html
-    /// https://dova-s.jp/
-    /// </summary>
-    public static class AudioBGMConstant
-    {
-        // bu
-        // public static readonly string TopScene = "dova_bgm_01";       // 思い出は引き出しの中
-        // public static readonly string MainScene = "MusMus-BGM-013";   // judas
-        // public static readonly string BattleScene = "MusMus-BGM-168";   // Attack of the Cyborg Legion
-
-        // bu
-        // protected readonly Dictionary<ButtonType, string> buttonTypeDictionary = new ()
-        // {
-        //     { ButtonType.Decide, AudioSEConstant.DecideSE },
-        //     { ButtonType.Cancel, AudioSEConstant.CancelSE },
-        //     { ButtonType.Selected, AudioSEConstant.CursorOnSE },
-        //     { ButtonType.OnPointCard, AudioSEConstant.OnPointCardSE }
-        // };
     }
 }
