27533390adb82f21aa622d8fd829e9200caad92b
Renoman <idearaizers@gmail.com>
2025-01-13 21:05:24 +0900

    add

--- Assets/Siasm/Scripts/Foundation/Cursor/CursorManager.cs
+++ Assets/Siasm/Scripts/Foundation/Cursor/CursorManager.cs
@@ -1,7 +1,7 @@
 namespace Siasm
 {
     /// <summary>
-    /// NOTE: カーソルアイコンを指定のものに変更する際に使用予定
+    /// カーソルアイコンを指定のものに変更する際に使用予定
     /// </summary>
     public class CursorManager : SingletonMonoBehaviour<CursorManager>
     {
