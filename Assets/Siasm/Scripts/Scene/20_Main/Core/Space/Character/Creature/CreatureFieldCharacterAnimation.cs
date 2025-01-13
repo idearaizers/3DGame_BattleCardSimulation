b62bb1a353cd0b1fa5b8bbf1e4f54eaf5b335ce3
Renoman <idearaizers@gmail.com>
2025-01-13 22:34:49 +0900

    add

--- Assets/Siasm/Scripts/Scene/20_Main/Core/Space/Character/Creature/CreatureFieldCharacterAnimation.cs
+++ Assets/Siasm/Scripts/Scene/20_Main/Core/Space/Character/Creature/CreatureFieldCharacterAnimation.cs
@@ -6,7 +6,7 @@ namespace Siasm
         public static readonly string StateNameRun = "Run";
         public static readonly string StateNameDash = "Dash";
         public static readonly string StateNameDeath = "Death";
-        public static readonly string StateNameHurt = "Hurt";  // テイクダメージ？
+        public static readonly string StateNameHurt = "Hurt";
         public static readonly string StateNameAttack01 = "Attack01";
         public static readonly string StateNameAttack02 = "Attack02";
         public static readonly string StateNameAttack03 = "Attack03";
